1b51f2f0bf784444b3dda8175311ce104885f52e
Ali Ka <kalikibbel@gmail.com>
2020-08-14 23:58:17 -0700

    added no jekyll file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
-const prod = process.env.NODE_ENV === 'production'
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/karisme.github.io' : ''
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+    }
+  },
+  //assetPrefix: '',
+  assetPrefix: !debug ? 'https://karisme.github.io/home' : '',
+  // webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    // Important: return the modified config
+    // return config
+  // }
 }
