d25ed50000c3bf97ec7a3562980160260de0a820
Ali Ka <kalikibbel@gmail.com>
2020-08-25 23:49:52 -0700

    css changes, looking into navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/global.css"
+import "../styles/global-dark.css"
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -6,45 +6,62 @@ import TypeIt from "typeit-react";
 import Date from '../components/date'
 import Baffle from "baffle-react";
 
+
 export default function Home() {
   return (
     <>
-    <Baffle
-        speed={125}
-        characters="█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░"
-        exclude={[" ", "!"]}
-        obfuscate={false}
-        revealDuration={5000}
-      > 
-        I'd like to add baffling because it looks cool. 
-    </Baffle>
+    
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
         <link rel="icon" type="image/x-icon" href="../public/favicon.ico" />
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          <TypeIt 
+        <div className={utilStyles.intro}>
+          Hello! My name is Ali, I'm
+          <TypeIt
             options={{
               lifelike: true,
               loop: true
             }}
             getBeforeInit={instance => {
               instance
-                .type("Wannabe Software Engineer, trying to make it. Don't ev")
-                .pause(250)
-                .delete(2)
-                .pause(200)
-                .type("@ me after the come up");
+                .type(" a Software Engineer")
+                .pause(350)
+                .delete(19)
+                .pause(300)
+                .type("Senegalese")
+                .pause(300)
+                .delete(10)
+                .pause(300)
+                .type("a Student")
+                .pause(300)
+                .delete(9)
+                .pause(300)
+                .type("a Hacker")
+                .pause(300)
+                .delete(8)
+                .pause(300)
+                .type("an Anime addict")
           
               return instance;
             }}
-          />
-        </p>
+          ></TypeIt>
+        </div>
       </section>
+      <br />
+      <br />
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Featured Projects</h2>
+        <h2 className={utilStyles.headingLg}>
+          <Baffle
+            speed={125}
+            characters="█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░"
+            exclude={[" ", "!"]}
+            obfuscate={false}
+            revealDuration={5000}
+            > Featured Projects
+          </Baffle> 
+        </h2>
       </section>
     </Layout>
     </>
